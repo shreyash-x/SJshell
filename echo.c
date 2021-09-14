f001001e91d55eaf426aef9bbcf510a5fa6ddacd
shreyash-x <shreyashjain2002@gmail.com>
2021-09-14 18:23:09 +0530

    Added functionality to handle commands separated by ;

--- echo.c
+++ echo.c
@@ -4,7 +4,7 @@ void get_echo(char input_tokens[][INT_MAX], int size)
 {
     for (int i = 1; i < size; i++)
     {
-        printf("%s", input_tokens[i]);
+        printf("%s ", input_tokens[i]);
     }
     printf("\n");
 }
--- main.c
+++ main.c
@@ -9,39 +9,55 @@ int main()
     while (1)
     {
         char *input_string = NULL;
-        char input_tokens[100][INT_MAX];
-        size_t n = 0;
 
         // Generating Prompt
         get_prompt(home_path);
 
         // Taking Input
+        size_t n = 0;
         getline(&input_string, &n, stdin);
         input_string[strlen(input_string) - 1] = '\0';
 
-        // Tokenizing Input String
-        int itr = 0;
-        char delimiter[2] = " ";
-        char *token;
-        token = strtok(input_string, delimiter);
-        while (token != NULL)
+        // Extracting commands from input string
+        char commands[100][INT_MAX];
+        int num_cmds = 0;
+        char delim[2] = ";";
+        char *cmd;
+        cmd = strtok(input_string, delim);
+        while (cmd != NULL)
         {
-            strcpy(input_tokens[itr], token);
-            itr++;
-            token = strtok(NULL, delimiter);
+            strcpy(commands[num_cmds], cmd);
+            num_cmds++;
+            cmd = strtok(NULL, delim);
         }
 
-        if (strcmp(input_tokens[0], "echo") == 0)
+        for (int i = 0; i < num_cmds; i++)
         {
-            get_echo(input_tokens, itr);
-        }
-        else if (strcmp(input_tokens[0], "pwd") == 0)
-        {
-            get_pwd();
-        }
-        else if (strcmp(input_tokens[0], "cd") == 0)
-        {
-            exec_cd(input_tokens[1], itr);
+            // Tokenizing command
+            char input_tokens[100][INT_MAX];
+            int itr = 0;
+            char delimiter[2] = " ";
+            char *token;
+            token = strtok(commands[i], delimiter);
+            while (token != NULL)
+            {
+                strcpy(input_tokens[itr], token);
+                itr++;
+                token = strtok(NULL, delimiter);
+            }
+
+            if (strcmp(input_tokens[0], "echo") == 0)
+            {
+                get_echo(input_tokens, itr);
+            }
+            else if (strcmp(input_tokens[0], "pwd") == 0)
+            {
+                get_pwd();
+            }
+            else if (strcmp(input_tokens[0], "cd") == 0)
+            {
+                exec_cd(input_tokens[1], itr);
+            }
         }
     }
 }
